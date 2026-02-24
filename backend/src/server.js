fe2d19ccad932cea1b6b42aa27f845307477a6a4
Nikolisditylis <nikosstavrakis2000@gmail.com>
2026-02-24 20:29:34 +0200

    adding last missing package

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ app.use('/api', footballRouter);
 app.use(unknownEndpoint)
 app.use(errorHandler);
 
-app.listen(3001, () => {
-  console.log('Backend running on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
